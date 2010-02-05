ee33cc7e75e5bfdfb07557db67dd16b017a1bb44
Henrique Carvalho Alves <hcarvalhoalves@gmail.com>
2010-02-05 00:21:49 -0200

    Fixed setup.py for templates

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import ez_setup
 ez_setup.use_setuptools()
-from setuptools import setup, find_packages
+from setuptools import setup
+
 setup(
     name = "django-landing",
     version = "0.1",
-    packages = find_packages(),
+    packages = ['landing'],
     author = "Henrique Carvalho Alves",
     author_email = "hcarvalhoalves@gmail.com",
     description = "Landing, a simple A/B testing application for Django",
     url = "http://github.com/hcarvalhoalves/django-landing",
-    include_package_data = True
+    include_package_data = True,
+    package_data = {'landing': ['templates/*']},
 )
